b85a057904984aad700132c17d3ced712372e3a1
astra-void <syntaxalchemist0@gmail.com>
2025-06-26 19:22:44 +0900

    tired

--- src/react/login.ts
+++ src/react/login.ts
@@ -3,26 +3,37 @@ import { User } from "./hooks";
 import { AdapterUser } from "../adapters";
 import { authRequest } from "./utils";
 
-export async function login(provider: string, params: LoginParams): Promise<User | AdapterUser | null> {
-    try {
-        const { email, password, redirect = true, redirectUrl = '/' } = params;
-        const req = await authRequest<User | AdapterUser | null>(
-            "POST", 
-            "/api/auth/login", 
-            { email, password, provider }
-        );
+export async function login(
+  provider: string, 
+  params?: LoginParams
+): Promise<User | AdapterUser | null> {
+  try {
+    const redirect = params?.redirect ?? true;
+    const redirectUrl = params?.redirectUrl ?? '/';
 
-        if (!req?.data || req.status !== 200) {
-            return null;
-        }
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const body: Record<string, any> = { provider };
 
-        if (redirect) {
-            window.location.href = redirectUrl
-            return null;
-        }
-        
-        return req.data;
-    } catch {
-        return null;
+    if (params?.email) body.email = params.email;
+    if (params?.password) body.password = params.password;
+
+    const req = await authRequest<User | AdapterUser | null>(
+      "POST",
+      "/api/auth/login",
+      body
+    );
+
+    if (!req?.data || req.status !== 200) {
+      return null;
     }
+
+    if (redirect) {
+      window.location.href = redirectUrl;
+      return null;
+    }
+
+    return req.data;
+  } catch {
+    return null;
+  }
 }
