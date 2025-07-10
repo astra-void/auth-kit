710c80e1e55eab141c7bdc09b3c2d84855f6fa80
astra-void <syntaxalchemist0@gmail.com>
2025-07-11 08:23:53 +0900

    ye

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'tsup'
 export default defineConfig({
     entry: ['src/**/*.ts'],
     outDir: 'dist',
-    format: ['esm', 'cjs'],
+    format: ['esm'],
     dts: false,
     minify: true,
     clean: true,
